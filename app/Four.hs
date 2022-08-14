02dfb3737e30183b11d93551f2a456dd6eacf369
Ivannick de Freijtas <nicky_defreytas@hotmail.com>
2022-08-14 23:32:33 +0200

    -Added usefull packages -Added player vs computer routine with random number

--- app/Four.hs
+++ app/Four.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Four where
 
@@ -10,10 +11,15 @@ import Data.List ()
 import Data.Maybe as Maybe ( Maybe(Just, Nothing) )
 import Data.Sequence (fromList)
 import qualified Data.Sequence as Seq
-import System.IO ()
--- import System.Random
--- import Control.Monad.State
-import Control.Lens ( (^?), element )
+import System.IO (hSetBuffering, stdin, BufferMode (NoBuffering))
+import System.Random (getStdGen, Random (randomRs), randomRIO, randomIO)
+import Control.Monad.State (lift)
+import Control.Lens ( (^?), element, (^.))
+import Control.Lens.Tuple ()
+import Unicode.Char ()
+import Text.Show.Unicode ()
+import Control.Concurrent ( threadDelay )
+
 
 
 --TYPES/DATA
@@ -22,10 +28,17 @@ data Disc = X | O | E deriving (Eq, Read, Show)
 type Row = [Disc]
 type Board = [[Disc]]
 data Maybe a = Nothing | Just a
+data Game = Game
+  { _gPlayerScore   :: Int
+  , _gComputerScore :: Int
+  } deriving Show
 
 -- PURE FUNCTIONS
 createBoard :: Size -> Size -> Board
 createBoard m n = replicate m (replicate n E)
+
+randomRsIO :: (Random a) => (a, a) -> IO [a]
+randomRsIO range = getStdGen >>= return . (randomRs range)
 
 switchPlayer :: Char -> Char
 switchPlayer 'X' = 'O'
@@ -58,28 +71,38 @@ updateBoard p i (xs : xss) = case xs ^? element i of
 
 -- IMPURE FUNCTIONS
 initRow :: Row -> IO ()
-initRow []  = putStrLn "There are no rows inserted" 
+initRow []  = putStrLn "There are no rows inserted"
 initRow [x] = putChar (showDisc $ head [x]) >> putStr "\n"
 initRow (x : xs) = putChar (showDisc $ head [x]) >> putChar ' ' >> initRow xs
 
 showBoard :: Board -> IO ()
 showBoard = mapM_ initRow
 
-playGame :: Char -> Board -> IO ()
+playGameP :: Int -> Int -> Char -> Board -> IO ()
+playGameP m r p b = do
+    putStr (showPlayerName p) >> putStr " choose input column: "
+    i :: Int <- readLn
+    let rb = reverse b
+    let em = updateBoard p i rb
+    if em == rb then (do
+        putStrLn "Column is full, please choose another column"
+        playGameP m r p rb) else showBoard $ reverse $ updateBoard p i rb
+    let rbr2 = reverse $ updateBoard p i rb
+    if m == 1 then playGameP m r (switchPlayer p) rbr2 else playGameC m r (switchPlayer p)  rbr2
 
-playGame p b = do
-  putStr (showPlayerName p) >> putStr " choose input column: "
-  i :: Int <- readLn
+playGameC :: Int -> Int -> Char -> Board -> IO ()
+playGameC m r p b = do
+  threadDelay 1000000
+  number <- randomRIO (0,r-1) :: IO Int
+  putStr "Computer made move in column:" >> print number
   let rb = reverse b
-  let em = updateBoard p i rb
-  if em == rb then (do
-      putStrLn "Column is full, please choose another column"
-      playGame p rb) else showBoard $ reverse $ updateBoard p i rb
-  let rbr2 = reverse $ updateBoard p i rb
-  playGame (switchPlayer p) rbr2
+  showBoard $ reverse $ updateBoard p number rb
+  let rbr2 = reverse $ updateBoard p number rb
+  playGameP m r (switchPlayer p) rbr2
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
   putStrLn "Choose a game mode: "
   putStrLn "1 -> Player vs Player"
   putStrLn "2 -> Player vs Computer"
@@ -92,7 +115,7 @@ main = do
   r :: Int <- readLn
   -- create board
   showBoard $ createBoard c r
-  playGame 'X' $ createBoard c r
+  playGameP m r 'X' $ createBoard c r
   return ()
 
 
--- app/TTT.hs
+++
@@ -1,1 +0,0 @@
-module TTT where
